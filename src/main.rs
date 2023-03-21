ceabde853dbbad72aa96c58eb1430efd24ff7f5a
TMO <tmo@mtb111.fr>
2023-03-21 15:43:21 +0100

    retreive api keys from files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::error::Error;
+use std::fs;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let public_api_key = fs::read_to_string("api_key.txt").expect("Should have been able to read the file");
+    let private_api_key = fs::read_to_string("private_api_key.txt").expect("Should have been able to read the file");
     let resp = reqwest::get("https://httpbin.org/ip")
         .await?
         .text()
